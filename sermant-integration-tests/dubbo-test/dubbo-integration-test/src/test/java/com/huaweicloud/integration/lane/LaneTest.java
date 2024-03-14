9dc24da6e219a8c0f244aa9240d94a61e439f324
chengyouling <spadgerlin888@163.com>
2024-03-14 15:06:04 +0800

    modify

--- sermant-integration-tests/dubbo-test/dubbo-integration-test/src/test/java/com/huaweicloud/integration/lane/LaneTest.java
+++ sermant-integration-tests/dubbo-test/dubbo-integration-test/src/test/java/com/huaweicloud/integration/lane/LaneTest.java
@@ -25,6 +25,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -47,16 +48,21 @@ public class LaneTest {
 
     private final String baseUrl;
 
+    private final boolean isExecuteSpringTest;
+
     /**
      * 构造方法
      */
     public LaneTest() {
         baseUrl = "http://127.0.0.1:" + System.getProperty("controller.port", "28019") + "/controller/getLaneBy";
+        isExecuteSpringTest = Boolean.parseBoolean(System.getProperty("execute.spring.test", "true"));
     }
 
     @Test
     public void testDubbo() {
-        testBySpring("Dubbo");
+        if (isExecuteSpringTest){
+            testBySpring("Dubbo");
+        }
         testByDubbo("Dubbo");
 
         // 正常染色（测试dubbo取值策略）
@@ -114,13 +120,17 @@ public class LaneTest {
 
     @Test
     public void testFeign() {
-        testBySpring("Feign");
+        if (isExecuteSpringTest){
+            testBySpring("Feign");
+        }
         testByDubbo("Feign");
     }
 
     @Test
     public void testRest() {
-        testBySpring("Rest");
+        if (isExecuteSpringTest){
+            testBySpring("Rest");
+        }
         testByDubbo("Rest");
     }
 
